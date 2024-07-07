2e52788679d7321f75eb1ca591cd6a251a7942d0
Sergio <147100454+sdevsantiago@users.noreply.github.com>
2024-07-07 09:57:50 +0200

    fix: C Piscine/C00/ft_print_reverse_alphabet function name now matches the file name

--- src/C Piscine/c00/ex02/ft_print_reverse_alphabet.c
+++ src/C Piscine/c00/ex02/ft_print_reverse_alphabet.c
@@ -6,14 +6,14 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:45:19 by sede-san          #+#    #+#             */
-/*   Updated: 2024/07/03 21:08:24 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/07/07 09:55:14 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 /* Prints the entire alphabet in reverse in undercase letters. */
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char	*rev_alphabet;
 
