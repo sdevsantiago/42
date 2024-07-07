0b102e5090befa3c551d0db48e09958c5da76097
Sergio <147100454+sdevsantiago@users.noreply.github.com>
2024-07-07 10:12:56 +0200

    fix: C Piscine/C01/ft_rev_int_tab loop should work

--- src/C Piscine/c01/ex07/ft_rev_int_tab.c
+++ src/C Piscine/c01/ex07/ft_rev_int_tab.c
@@ -6,24 +6,38 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:14:53 by sede-san          #+#    #+#             */
-/*   Updated: 2024/07/04 19:56:59 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/07/07 10:07:07 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_rev_int_tab(int *tab, int size)
+void	ft_rev_int_tab(int *tab, int size)
 {
-	int array[size];
 	int	i;
+	int	aux;
 
-	i = size - 1;	
-	while (i-- >= 0)
-		array
+	i = 0;
+	while (i <= (size / 2) - 1)
+	{
+		aux = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = aux;
+		i++;
+	}
 }
 
 int main(void)
 {
-	
+	int	nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+	ft_rev_int_tab(nums, 10);
+	int		i = 0;
+	char	numero;
+	while (nums[i] != '\0')
+	{
+		numero = nums[i] + '0';
+		write(1, &numero, 1);
+		i++;
+	}
 	return 0;
 }
