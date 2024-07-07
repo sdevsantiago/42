dd6004bceb30f1f05539bda1b2b926d2caa68891
Sergio De Santiago Redondo <sede-san@c3r2s3.42madrid.com>
2024-07-07 12:40:23 +0200

    fix: (C Piscine/C01/ex06/ft_strlen.c) the return value is now correct

--- src/C Piscine/c01/ex06/ft_strlen.c
+++ src/C Piscine/c01/ex06/ft_strlen.c
@@ -6,15 +6,15 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:13:56 by sede-san          #+#    #+#             */
-/*   Updated: 2024/07/04 19:39:45 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/07/07 12:36:52 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
 	int	i;
-	
-	i = 0;
+
+	i = 1;
 	while (str[i] != '\0')
 		i++;
 	return (i);
@@ -22,6 +22,6 @@ int	ft_strlen(char *str)
 
 // int main(void)
 // {
-// 	int length = ft_strlen("Hola mundo");
+// 	int length = ft_strlen("¡Hola mundo!");
 // 	return 0;
 // }
