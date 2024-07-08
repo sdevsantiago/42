eaf7893ffed3c060eb4b71f83eba846cc09fff93
Sergio De Santiago Redondo <sede-san@c3r3s5.42madrid.com>
2024-07-08 17:34:25 +0200

    add: C Piscine/C02/ex04/ft_str_is_lowercase.c

---
+++ src/C Piscine/c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 17:31:38 by sede-san          #+#    #+#             */
+/*   Updated: 2024/07/08 17:33:58 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	Checks if the string passed contains only lowercase characters.
+	- #### Parameters
+	-	char *str: The string to evaluate
+	- #### Return
+	-	Returns 1 if the string contains only lowercase characters or if the
+		string passed is empty, if not, returns 0.
+*/
+int	ft_str_is_lowercase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'a' || str[i] > 'z')
+			return (0);
+		i++;
+	}
+	return (1);
+}
