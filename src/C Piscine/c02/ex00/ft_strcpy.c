ac6055b489cf45193b1b5d6c1da45fc117a28069
Sergio De Santiago Redondo <sede-san@c3r3s5.42madrid.com>
2024-07-08 16:44:57 +0200

    fix: (C Piscine/C02/ex00) The function now introduces the '\0' character in the last index

--- src/C Piscine/c02/ex00/ft_strcpy.c
+++ src/C Piscine/c02/ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 17:07:00 by sede-san          #+#    #+#             */
-/*   Updated: 2024/07/07 17:46:46 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/07/08 16:42:17 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 	- #### Parameters
 	-	char *dest: String to copy char
 	-	char *src: String where copy
+	- #### Return
+	-	The copied string.
 */
 char	*ft_strcpy(char *dest, char *src)
 {
 	unsigned int	i;
-	
+
+	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	src[i] = '\0';
 	return (dest);
 }
