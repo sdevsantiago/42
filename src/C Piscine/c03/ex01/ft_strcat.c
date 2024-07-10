c2f3df262e6af1042a6ffd23a169409bb7bfb0fb
Sergio De Santiago Redondo <sede-san@c2r9s5.42madrid.com>
2024-07-10 07:28:59 +0200

    add: C Piscine/c03/ex01/ft_strcat.c

---
+++ src/C Piscine/c03/ex01/ft_strcat.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 05:27:30 by sede-san          #+#    #+#             */
+/*   Updated: 2024/07/10 07:24:49 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	Measures a strings length.
+	- #### Parameter
+	-	char *str: The string to measure.
+	- #### Return
+	-	Returns the length of the passed string.
+*/
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*
+	Concatenate two strings, appending 'dest' to 'src'.
+	- #### Parameters
+	-	char *dest: String to concatenate to
+	-	char *src: String to concatenate from
+	- #### Return
+	-	Returns the concatenated string.
+*/
+char	*ft_strcat(char *dest, char *src)
+{
+	unsigned int	i;
+	unsigned int	j;
+	
+	i = ft_strlen(dest);
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
