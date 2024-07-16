e169fea96d22c4950fd3e398e4c69e7421bc9aa7
Sergio de Santiago Redondo <sede-san@student.42madrid.com>
2024-07-16 07:35:44 +0200

    fix: (C Piscine/c03/ex01/ft_strncmp.c) Function now returns 0 if n is 0

--- src/C Piscine/c03/ex01/ft_strncmp.c
+++ src/C Piscine/c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 08:21:30 by sede-san          #+#    #+#             */
-/*   Updated: 2024/07/10 19:33:06 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/07/16 07:21:20 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,32 @@
 	-	char *s2: The other string
 	-	unsigned int n: The character up to where compare
 	- #### Return
-	-	Returns 0 if both strings are equal, a positive value if the first
-		string is bigger than the second and a negative value if the second
-		string is bigger than the first.
+	-	Returns 0 if both strings are equal or if n equals 0, a positive value
+		if the first string is bigger than the second and a negative value if
+		the second string is bigger than the first.
 */
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && n > 0 && (s1[i] != '\0' && s2[i] != '\0'))
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0') && n > 0)
 	{
 		i++;
 		n--;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	*s1 = "\201";
+	char	*s2 = "\101";
+	printf("Original strncmp:\t%d\n", strncmp(s1, s2, 2));
+	printf("My strncmp:\t\t%d\n", ft_strncmp(s1, s2, 2));
+	return (0);
 }
