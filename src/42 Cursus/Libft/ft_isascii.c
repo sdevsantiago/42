94b1c310c81e1a17a1b0dec8010b531dcadcf3d4
Sergio de Santiago Redondo <sede-san@student.42madrid.com>
2024-09-16 22:00:32 +0200

    add: Libft/ft_isascii.c

---
+++ src/42 Cursus/Libft/ft_isascii.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 19:38:09 by sede-san          #+#    #+#             */
+/*   Updated: 2024/09/16 21:49:24 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	Checks if the passed character is within the 7-bit ASCII character set.
+*/
+int	ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
