64da9f652ae473077035700bfcf171d232a39794
Sergio de Santiago Redondo <sede-san@student.42madrid.com>
2024-09-16 22:00:10 +0200

    add: Libft/ft_isalpha.c

---
+++ src/42 Cursus/Libft/ft_isalpha.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 18:55:36 by sede-san          #+#    #+#             */
+/*   Updated: 2024/09/16 19:29:20 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	Checks  if the passed number is a letter (uppercase or lowercase) 	
+*/
+
+int	ft_isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
