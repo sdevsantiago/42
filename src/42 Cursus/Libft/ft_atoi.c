09c05e5908096723af789f1696efa0479e4c3b63
Sergio de Santiago Redondo <sede-san@student.42madrid.com>
2024-09-16 21:59:16 +0200

    add: Libft/ft_atoi.c

---
+++ src/42 Cursus/Libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 17:37:46 by sede-san          #+#    #+#             */
+/*   Updated: 2024/09/16 20:04:57 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	Converts a string into an integer.
+*/
+int	ft_atoi(const char *str)
+{
+	int	minus;
+	int	n;
+
+	minus = 1;
+	n = 0;
+	while (ft_isspace(*str))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			minus = -minus;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		n = n * 10 + (*str - '0');
+		str++;
+	}
+	return (n * minus);
+}
