3f6f8938128aed6e3e4a2be2c973380a2a1bb391
sede-san <sede-san@student.42madrid.com>
2024-09-17 21:51:32 +0200

    update: Deleted all commented X marks

--- src/42 Cursus/Libft/libft.h
+++ src/42 Cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:40:58 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/16 22:03:49 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:39:27 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 
 # include <stdlib.h>
 
-int		ft_atoi(const char *str); // X
-int		ft_isalnum(int c); // X
-int		ft_isalpha(int c); // X
-int		ft_isascii(int c); // X
-int		ft_isdigit(int c); // X
-int		ft_isprint(int c); // X
-int		ft_isspace(int c); // X
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isspace(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strncmp(const char *s1, const char *s2, size_t n); // X
-int		ft_tolower(int c); // X
-int		ft_toupper(int c); // X
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s); // X
+char	*ft_strdup(const char *s);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(const char *s); // X
+size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
