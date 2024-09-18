afc1ba03d68eeeb211f6c885bdb557bd7758b6b2
sede-san <sede-san@student.42madrid.com>
2024-09-18 20:55:18 +0200

    add: Libft/ft_memcmp.c

---
+++ src/42 Cursus/Libft/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 20:22:42 by sede-san          #+#    #+#             */
+/*   Updated: 2024/09/18 20:54:16 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	Compares the first n bytes of the memory areas s1 and s2.
+*/
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*d1 = s1;
+	const unsigned char	*d2 = s2;
+	size_t				i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i > n || d1[i] == d2[i])
+		i++;
+	return (d1[i] - d2[i]);
+}
