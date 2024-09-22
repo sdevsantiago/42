a98090dc70be48a0e48d87fbe675966f080c91f1
Sergio de Santiago <147100454+sdevsantiago@users.noreply.github.com>
2024-09-22 21:56:23 +0200

    fix: fixed a compilation error

--- src/42 Cursus/Libft/ft_isalpha.c
+++ src/42 Cursus/Libft/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:55:36 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/20 19:38:38 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/22 21:55:38 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 /* Checks  if the passed number is a letter (uppercase or lowercase).  */
 int	ft_isalpha(int c)
 {
-	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
