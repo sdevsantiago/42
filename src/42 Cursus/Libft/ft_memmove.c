3c48741a5115696088e433c44c71480aa92d596c
Sergio de Santiago <147100454+sdevsantiago@users.noreply.github.com>
2024-09-22 16:03:57 +0200

    fix: function didn't work as expected and copied the functionality of memcpy

--- src/42 Cursus/Libft/ft_memmove.c
+++ src/42 Cursus/Libft/ft_memmove.c
@@ -6,25 +6,24 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 14:35:31 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/22 02:09:17 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/22 16:00:16 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Copies N bytes from memory area SRC to memory area DEST.  */
+/* Copies N bytes from memory area SRC to memory area DEST.
+   This function is protected against overlapping.  */
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*s;
-	char	*d;
+	unsigned char	*s;
+	unsigned char	*d;
 
-	s = (char *)src;
-	d = (char *)dest;
+	if (!((dest > src && dest < src + n) || (src > dest && src < dest + n)))
+		return (ft_memcpy(dest, src, n));
+	s = (unsigned char *)src + n - 1;
+	d = (unsigned char *)dest + n - 1;
 	while (n--)
-	{
-		*d = *s;
-		d++;
-		s++;
-	}
+		*d-- = *s--;
 	return (dest);
 }
