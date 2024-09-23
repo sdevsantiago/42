18f9b8bcc51d198b027c1581a5982212f99ef217
Sergio de Santiago <147100454+sdevsantiago@users.noreply.github.com>
2024-09-23 19:46:04 +0200

    update: detailed function description

--- src/42 Cursus/Libft/ft_calloc.c
+++ src/42 Cursus/Libft/ft_calloc.c
@@ -6,13 +6,14 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 21:17:43 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/23 09:27:26 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/23 15:30:50 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Allocates memory for an array of NMEMB elements of SIZE bytes each.  */
+/* Allocates memory for an array of NMEMB elements of SIZE bytes each.
+   Each byte is set to 0.  */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*c;
