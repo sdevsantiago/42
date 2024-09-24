bad957b2a4ffd2b5f775d26599cfd0e631086b69
sede-san <sede-san@student.42madrid.com>
2024-09-24 16:22:40 +0200

    fix: the function used an alias instead of the command

--- tools/ft_getmakefilesrcs.sh
+++ tools/ft_getmakefilesrcs.sh
@@ -2,11 +2,11 @@
 
 # Gets all the files in the current directory and adds the corresponding field in the Makefile file, if exists.
 ft_getmakefilesrcs() {
-	RED='\033[0;31m'  # Color rojo
-    NC='\033[0m'      # Sin color (reset)
+	RED='\033[0;31m'
+    NC='\033[0m'
 	if [ ! -f ./Makefile ]; then
 		echo "${RED}error: Makefile not found${NC}"
 		return 1
 	fi
-	echo SRCS= | tr -d "\n" >> Makefile && la | grep -v "Makefile" | sed 's/$/ \\/' >> Makefile
+	echo SRCS= | tr -d "\n" >> Makefile && ls -la | grep -v "Makefile" | sed 's/$/ \\/' >> Makefile
 }
