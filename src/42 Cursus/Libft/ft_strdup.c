9bbadafab636d1db699797c5af72b5fc4f287304
sede-san <sede-san@student.42madrid.com>
2024-09-25 17:56:53 +0200

    fix: ft_strdup did not finish the string with the NULL terminator

--- src/42 Cursus/Libft/ft_strdup.c
+++ src/42 Cursus/Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:33:49 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/20 19:39:29 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/25 17:52:03 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ char	*ft_strdup(const char *s)
 	char	*d;
 
 	d = (char *)malloc(ft_strlen(s) + 1);
+	if (!d)
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
 		d[i] = s[i];
 		i++;
 	}
+	d[i] = '\0';
 	return (d);
 }
