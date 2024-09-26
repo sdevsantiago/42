684214671ebe0dce173261ffc634a1f503d51fb4
sede-san <sede-san@student.42madrid.com>
2024-09-26 21:02:21 +0200

    fix: ft_strrchr.c did not take into account the case in where the caracter was in the index 0 of the string

--- src/42 Cursus/Libft/ft_strrchr.c
+++ src/42 Cursus/Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:34:38 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/22 21:09:25 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/26 21:01:18 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 /* Finds the last ocurrence of C in the string S.  */
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	l;
+	short	l;
 
 	l = ft_strlen(s);
 	s += l;
 	if (*s == '\0' && c == '\0')
 		return ((char *)s);
-	while (l--)
+	while (l-- >= 0)
 	{
 		if (*s == c)
 			return ((char *)s);
