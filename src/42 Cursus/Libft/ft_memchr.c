4dc2d9aa6e23552d98f9e070c6ff518efc1b1703
sede-san <sede-san@student.42madrid.com>
2024-09-26 21:33:46 +0200

    fix: fixed an error in where ft_memchr.c did not return NULL in case the scanned bytes did not match what was being searched

--- src/42 Cursus/Libft/ft_memchr.c
+++ src/42 Cursus/Libft/ft_memchr.c
@@ -6,23 +6,23 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:52:33 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/22 18:36:34 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/26 21:30:10 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* Scans the initial N bytes of the memory area pointed to by S for the first
+   instance of C.  */
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*d = (unsigned char *)s;
 
 	while (n--)
 	{
-		if (*d == c)
-		{
-			s = d;
-			return (s);
-		}
+		if (*d == (unsigned char)c)
+			return ((void *)d);
 		d++;
 	}
+	return (NULL);
 }
