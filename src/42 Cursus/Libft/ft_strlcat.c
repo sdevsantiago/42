72606f088aa9e0e2c132fa7099b711899d509cc9
sede-san <sede-san@student.42madrid.com>
2024-09-27 19:36:19 +0200

    fix: fixed an error in where the function did not work with strings not ending in NULL

--- src/42 Cursus/Libft/ft_strlcat.c
+++ src/42 Cursus/Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 12:59:02 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/26 20:09:16 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/09/27 19:34:40 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	d;
 	size_t	s;
 
-	d = ft_strlen(dst);
+	d = 0;
+	while (dst && dst[d] && d < size)
+		d++;
 	s = ft_strlen(src);
 	if (size <= d)
 		return (size + s);
 	size -= d;
 	dst += d;
 	while (size-- > 1 && *src)
-	{
-		*dst = *src;
-		dst++;
-		src++;
-	}
+		*dst++ = *src++;
 	*dst = '\0';
 	return (d + s);
 }
