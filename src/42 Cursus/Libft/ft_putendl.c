13cf6de0f230c83c8086857628ae11d2273e377d
sede-san <sede-san@student.42madrid.com>
2024-10-01 20:45:03 +0200

    fix: ft_putendl used ft_putstr_fd and ft_putchar_fd

--- src/42 Cursus/Libft/ft_putendl.c
+++ src/42 Cursus/Libft/ft_putendl.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:55:02 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/01 20:02:50 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/01 20:43:34 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 /* Prints a string followed by a new line.  */
 void	ft_putendl(char *s)
 {
-	ft_putstr_fd(s, STDOUT_FILENO);
-	ft_putchar_fd('\n', STDOUT_FILENO);
+	ft_putendl_fd(s, STDOUT_FILENO);
 }
