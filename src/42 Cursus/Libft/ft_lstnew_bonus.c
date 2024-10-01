2ca3c896cf5d1849702faa4a31317f0e408247df
sede-san <sede-san@student.42madrid.com>
2024-10-01 21:19:15 +0200

    add: Libft/ft_lstnew_bonus.c

---
+++ src/42 Cursus/Libft/ft_lstnew_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 21:05:25 by sede-san          #+#    #+#             */
+/*   Updated: 2024/10/01 21:15:57 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*l;
+
+	l = (t_list *)malloc(sizeof(t_list));
+	if (!l)
+		return (NULL);
+	l->content = content;
+	l->next = NULL;
+}
