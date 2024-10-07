3ae681c7455a4c1fb6de50deb57755fa397f383a
sede-san <sede-san@student.42madrid.com>
2024-10-07 19:26:29 +0200

    fix: function was returning NULL when id didn't have to

--- src/42 Cursus/Libft/ft_strrchr.c
+++ src/42 Cursus/Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:34:38 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/26 21:01:18 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/07 19:22:15 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 /* Finds the last ocurrence of C in the string S.  */
 char	*ft_strrchr(const char *s, int c)
 {
-	short	l;
+	char	*last;
 
-	l = ft_strlen(s);
-	s += l;
-	if (*s == '\0' && c == '\0')
-		return ((char *)s);
-	while (l-- >= 0)
+	last = NULL;
+	while (*s)
 	{
-		if (*s == c)
-			return ((char *)s);
-		s--;
+		if (*s == (char)c)
+			last = (char *)s;
+		s++;
 	}
-	return (NULL);
+	if (*s == (char)c)
+		return ((char *)s);
+	return (last);
 }
