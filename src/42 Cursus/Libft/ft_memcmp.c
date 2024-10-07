86c34c0612fd4a3676528480f045097e9927c4a0
sede-san <sede-san@student.42madrid.com>
2024-10-07 19:52:11 +0200

    fix: fixed comment typo

--- src/42 Cursus/Libft/ft_memcmp.c
+++ src/42 Cursus/Libft/ft_memcmp.c
@@ -6,13 +6,13 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:22:42 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/03 21:32:25 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/07 19:51:54 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Compares the first n bytes of the memory areas s1 and s2.  */
+/* Compares the first N bytes of the memory areas S1 and S2.  */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*d1 = s1;
@@ -24,7 +24,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n && d1[i] == d2[i] && (d1[i] && d2[i]))
 		i++;
-	if (i == n || !d1[i] || !d2[i])
-		i--;
 	return (d1[i] - d2[i]);
 }
