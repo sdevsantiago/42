921428b2406065af214d2958758f6d8d53a76a9f
sede-san <sede-san@student.42madrid.com>
2024-10-08 21:09:44 +0200

    update: simplified variable names

--- src/42 Cursus/Libft/ft_lstnew_bonus.c
+++ src/42 Cursus/Libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 21:05:25 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/08 14:43:44 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:15:44 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 /* Creates a new list with the value CONTENT.  */
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new_list;
+	t_list	*node;
 
-	new_list = (t_list *)malloc(sizeof(t_list));
-	if (!new_list)
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
 		return (NULL);
-	new_list->content = content;
-	new_list->next = NULL;
-	return (new_list);
+	node->content = content;
+	node->next = NULL;
+	return (node);
 }
