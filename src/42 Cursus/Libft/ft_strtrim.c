7b1e3899757abe9a43f0081fa84db1cd8d79b218
sede-san <sede-san@student.42madrid.com>
2024-10-08 20:53:41 +0200

    fix: function was removing characters from NULL terminator, which left the right border intact.

--- src/42 Cursus/Libft/ft_strtrim.c
+++ src/42 Cursus/Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 20:27:39 by sede-san          #+#    #+#             */
-/*   Updated: 2024/09/29 16:06:24 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:52:18 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
    string S1.  */
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	size_t	f;
-	size_t	l;
+	size_t	first;
+	size_t	last;
 
 	if (!s1 || !set)
 		return (NULL);
-	f = 0;
-	l = ft_strlen(s1) - 1;
-	while (s1[f] && ft_strchr(set, s1[f]))
-		f++;
-	while (s1[l] && ft_strchr(set, s1[l]))
-		l--;
-	return (ft_substr(s1, f, l - f + 1));
+	first = 0;
+	last = ft_strlen(s1) - 1;
+	while (s1[first] && ft_strchr(set, s1[first]))
+		first++;
+	while (s1[last] && ft_strchr(set, s1[last]))
+		last--;
+	return (ft_substr(s1, first, last - first + 1));
 }
