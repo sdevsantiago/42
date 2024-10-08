fe6a366b9fda3f07cc4bd28bd13d31a01df1a17f
sede-san <sede-san@student.42madrid.com>
2024-10-08 20:25:59 +0200

    fix: added NULL check

--- src/42 Cursus/Libft/ft_memcpy.c
+++ src/42 Cursus/Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 01:56:30 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/08 14:47:54 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:25:34 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*src_ptr = (unsigned char *)src;
 	unsigned char		*dest_ptr;
 
+	if (!dest && !src)
+		return (NULL);
 	dest_ptr = (unsigned char *)dest;
 	while (n--)
 		*dest_ptr++ = *src_ptr++;
