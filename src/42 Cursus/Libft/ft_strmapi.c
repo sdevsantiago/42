c0599e060fbfe5704306372242c21a6839114a2b
sede-san <sede-san@student.42madrid.com>
2024-10-08 20:58:54 +0200

    update: modified variable names to make it easier to read

--- src/42 Cursus/Libft/ft_strmapi.c
+++ src/42 Cursus/Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 20:44:16 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/01 16:11:14 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/08 15:07:09 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
    The passed string is not modified, all changes are applied to a copy.  */
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*str;
+	char	*str_mapped;
 	size_t	i;
 
-	str = ft_strdup(s);
-	if (!s || !f || !str)
+	str_mapped = ft_strdup(s);
+	if (!s || !f || !str_mapped)
 		return (NULL);
 	i = 0;
-	while (str[i])
+	while (str_mapped[i])
 	{
-		str[i] = f(i, str[i]);
+		str_mapped[i] = f(i, str_mapped[i]);
 		i++;
 	}
-	return (str);
+	return (str_mapped);
 }
