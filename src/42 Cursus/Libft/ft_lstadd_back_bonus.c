0aa3c2d4cdd74ce7236352521907495ac67445fd
sede-san <sede-san@student.42madrid.com>
2024-10-09 21:54:24 +0200

    update: modified variable names to specify purpose

--- src/42 Cursus/Libft/ft_lstadd_back_bonus.c
+++ src/42 Cursus/Libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 21:57:46 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/08 14:32:20 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/09 20:36:25 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /* Adds the node NEW to the last position of the list LST.  */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*aux_lst;
+	t_list	*lst_ptr;
 
 	if (!lst || !new)
 		return ;
@@ -23,8 +23,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 	else
 	{
-		aux_lst = ft_lstlast(*lst);
-		aux_lst->next = new;
+		lst_ptr = ft_lstlast(*lst);
+		lst_ptr->next = new;
 	}
 	new->next = NULL;
 }
