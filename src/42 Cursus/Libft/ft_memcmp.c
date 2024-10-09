03e446637a757135cf6713dce64100f78da7d57d
sede-san <sede-san@student.42madrid.com>
2024-10-09 18:57:07 +0200

    fix: function now works as expected

--- src/42 Cursus/Libft/ft_memcmp.c
+++ src/42 Cursus/Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:22:42 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/07 20:26:47 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:56:21 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 /* Compares the first N bytes of the memory areas S1 and S2.  */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*d1 = s1;
-	const unsigned char	*d2 = s2;
+	const unsigned char	*ptr_s1 = s1;
+	const unsigned char	*ptr_s2 = s2;
 	size_t				i;
 
-	if (!s1 || !s2 || n == 0)
+	if (!n)
 		return (0);
 	i = 0;
-	while (i < n && d1[i] == d2[i])
+	while (i < n - 1 && ptr_s1[i] == ptr_s2[i])
 		i++;
-	return (d1[i] - d2[i]);
+	return (ptr_s1[i] - ptr_s2[i]);
 }
