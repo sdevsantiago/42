b29d001950c45b686f23ce0f4d82714480352a77
sede-san <sede-san@student.42madrid.com>
2024-10-09 20:26:13 +0200

    fix: internal loop was causing errors

--- src/42 Cursus/Libft/ft_strnstr.c
+++ src/42 Cursus/Libft/ft_strnstr.c
@@ -6,34 +6,34 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 18:23:12 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/07 19:42:25 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/09 20:25:54 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Finds the substring LITTLE in LEN characters of the string BIG.  */
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	unsigned int	i;
 	unsigned int	j;
 
-	i = 0;
-	j = 0;
 	if (!(*little))
 		return ((char *)big);
-	while (big[i] && len-- + 1)
+	while (*big && len)
 	{
-		while ((big[i] == little[j]) && (big[i] || little[j]) && len-- + 1)
+		i = 0;
+		j = 0;
+		while (big[i] == little[j] && big[i] && little[j] && len)
 		{
 			i++;
 			j++;
+			len--;
 		}
-		if (!(little[j]))
+		len += j;
+		if (!little[j])
 			return ((char *)big);
 		big++;
-		i = 0;
-		j = 0;
+		len--;
 	}
 	return (NULL);
 }
