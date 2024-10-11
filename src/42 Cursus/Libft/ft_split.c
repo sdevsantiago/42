bafcab6c53311f4b23423da56268fd96687b9663
sede-san <sede-san@student.42madrid.com>
2024-10-11 20:09:53 +0200

    fix: fixed multiple errors

--- src/42 Cursus/Libft/ft_split.c
+++ src/42 Cursus/Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 14:57:37 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/08 14:58:57 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/11 20:09:27 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,33 @@ static size_t	count_words(const char *s, char c)
 {
 	size_t	words;
 
-	if (!s || !c)
+	if (!s)
 		return ((size_t) NULL);
 	words = 0;
 	while (*s)
 	{
 		while (*s == c && *s)
 			s++;
-		words++;
+		if (*s)
+			words++;
 		while (*s != c && *s)
 			s++;
 	}
 	return (words);
 }
 
-/* Frees the last R rows, from the M matrix.  */
-static void	free_matrix(void **m, size_t r)
+/* Frees an allocated matrix.  */
+static void	free_matrix(void **matrix)
 {
-	if (m && r)
+	size_t	row;
+
+	row = 0;
+	while (matrix[row])
 	{
-		while (r--)
-			free(m[r]);
-		free(m);
+		free(matrix[row]);
+		row++;
 	}
-}
-
-/* Allocates memory for the next row.  */
-static char	*calloc_row(char *s, char c)
-{
-	char	*row;
-
-	if (!ft_strchr(s, c))
-		row = ft_calloc(ft_strlen(s) + 1, sizeof(*row));
-	else
-		row = ft_calloc((ft_strchr(s, c) - s) + 1, sizeof(*row));
-	return (row);
+	free(matrix);
 }
 
 /* Splits the string S using C as separator.  */
@@ -59,25 +51,27 @@ char	**ft_split(const char *s, char c)
 {
 	char	**s_split;
 	size_t	row;
+	size_t	len;
 
-	s_split = ft_calloc(count_words(s, c) + 1, sizeof(*s_split));
-	if (!s || !c || !s_split)
+	if (!s)
 		return (NULL);
-	s += (long)ft_strchr(s, c);
+	s_split = (char **)malloc((count_words(s, c) + 1) * sizeof(*s_split));
+	if (!s_split)
+		return (NULL);
 	row = 0;
-	if (!*s)
-		*s_split = NULL;
 	while (*s)
 	{
-		s_split[row] = calloc_row((char *)s, c);
-		if (!s_split[row])
-			return (free_matrix((void **)s_split, row), NULL);
-		ft_strlcpy(s_split[row], s, ft_strchr(s, c) - s + 1);
-		while (*s && *s != c)
+		while (*s == c && *s)
 			s++;
-		while (*s && *s == c)
-			s++;
-		row++;
+		len = 0;
+		while (s[len] != c && s[len])
+			len++;
+		if (len)
+			s_split[row++] = ft_substr(s, 0, len);
+		if (!&s_split[row - 1])
+			return (free_matrix((void **)s_split), NULL);
+		s += len;
 	}
+	s_split[row] = NULL;
 	return (s_split);
 }
