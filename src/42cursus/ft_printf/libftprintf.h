251037be68dea6a75262b3ae26037520a62a5839
sede-san <sede-san@student.42madrid.com>
2024-10-30 19:52:55 +0100

    update(ft_printf): changed ft_print variable name and changed LLibft path

--- src/42cursus/ft_printf/libftprintf.h
+++ src/42cursus/ft_printf/libftprintf.h
@@ -6,16 +6,16 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 18:21:23 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/29 19:40:44 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/10/30 19:52:05 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include "lib/Libft/libft.h"
+# include "Libft/libft.h"
 # include <stdarg.h>
 
-int	ft_printf(char const *, ...);
+int	ft_printf(char const *format, ...);
 
 #endif
