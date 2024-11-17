8f0e795873f425acf26150c50a452be39fdfe341
sdevsantiago <147100454+sdevsantiago@users.noreply.github.com>
2024-11-17 13:39:30 +0100

    update(Libft): added new functions to libft.h and Makefile

--- src/42cursus/Libft/libft.h
+++ src/42cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:40:58 by sede-san          #+#    #+#             */
-/*   Updated: 2024/11/12 20:54:30 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/11/14 19:46:22 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,10 +86,13 @@ void	ft_putendl(char *s);
 void	ft_putnbr(int n);
 void	ft_putstr(char *s);
 void	ft_swap(void *a, void *b);
+int		ft_islower(int c);
 int		ft_isspace(int c);
+int		ft_isupper(int c);
 char	*ft_itoa_base(int n, const char *base);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 char	*ft_uitoa(unsigned int n);
 char	*ft_uitoa_base(unsigned int n, const char *base);
+char	*ft_ultoa_base(unsigned long n, const char *base);
 
 #endif
