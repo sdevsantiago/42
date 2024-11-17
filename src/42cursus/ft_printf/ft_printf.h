3d13eec927ce71fcee96e1a81d255254708f4541
sdevsantiago <147100454+sdevsantiago@users.noreply.github.com>
2024-11-17 13:42:00 +0100

    update(ft_printf): renamed libftprintf.h to ft_libft.h

--- src/42cursus/ft_printf/ft_printf.h
+++ src/42cursus/ft_printf/ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 18:21:23 by sede-san          #+#    #+#             */
-/*   Updated: 2024/11/05 20:17:38 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/11/15 10:20:58 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,17 @@
 # include <stdint.h>
 
 int	ft_printf(char const *format, ...);
+
+/* Character/String types */
+int	ft_printf_putchar(int const c);
+int	ft_printf_putstr(char const *str);
+
+/* Integer types */
+int	ft_printf_putint(int const n);
+int	ft_printf_putuint(unsigned int const n);
+int ft_printf_putuint_hex(unsigned int const n, const int c);
+
+/* Memdir types */
+int	ft_printf_putptr(intptr_t const ptr);
 
 #endif
