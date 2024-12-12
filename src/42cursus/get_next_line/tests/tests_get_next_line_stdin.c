164496f9bf2943607cce9599bb4a7a0a8f480109
sede-san <sede-san@student.42madrid.com>
2024-12-12 18:02:01 +0100

    feat(get_next_line): added stdin test

---
+++ src/42cursus/get_next_line/tests/tests_get_next_line_stdin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests_get_next_line_stdin.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 15:40:18 by sede-san          #+#    #+#             */
+/*   Updated: 2024/12/12 17:52:29 by sede-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../get_next_line.h"
+#include <signal.h>
+
+int main(void)
+{
+	char 			*line;
+	unsigned int	count = 0;
+
+    while (count++ < 5)
+    {
+		line = get_next_line(STDIN_FILENO);
+        write(STDOUT_FILENO, line, ft_strlen(line));
+        free(line);
+        line = NULL;
+    }
+
+    return (0);
+}
