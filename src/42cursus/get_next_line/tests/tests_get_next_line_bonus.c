f1b102d4d7a650f986a6d8e695e3a49a75925be4
sede-san <sede-san@student.42madrid.com>
2024-12-14 11:26:07 +0100

    feat(get_next_line): tester now indicates which file is reading

--- src/42cursus/get_next_line/tests/tests_get_next_line_bonus.c
+++ src/42cursus/get_next_line/tests/tests_get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:34:41 by sede-san          #+#    #+#             */
-/*   Updated: 2024/12/14 10:44:50 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/12/14 11:25:00 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define GREEN_TEXT "\033[32m"
 
 int main(int argc, char **argv)
 {
@@ -28,12 +31,13 @@ int main(int argc, char **argv)
 		printf("ERROR. Invalid number of arguments (must include 3 files)");
 		return (1);
 	}
-	
+
 	f1 = open(argv[1], O_RDONLY);
 	f2 = open(argv[2], O_RDONLY);
 	f3 = open(argv[3], O_RDONLY);
- 	
+
 	c = 0;
+	printf(BOLD GREEN_TEXT"FILE 1\n"RESET);
 	while (c < 10)
 	{
 		line = get_next_line(f1);
@@ -44,8 +48,9 @@ int main(int argc, char **argv)
 		c++;
 	}
 	printf("\n");
-	
+
 	c = 0;
+	printf(BOLD GREEN_TEXT"FILE 2\n"RESET);
 	while (c < 10)
 	{
 		line = get_next_line(f2);
@@ -56,7 +61,9 @@ int main(int argc, char **argv)
 		c++;
 	}
 	printf("\n");
+
 	c = 0;
+	printf(BOLD GREEN_TEXT"FILE 3\n"RESET);
 	while (c < 10)
 	{
 		line = get_next_line(f3);
@@ -67,7 +74,7 @@ int main(int argc, char **argv)
 		c++;
 	}
 	printf("\n");
-	
+
 	close(f1);
 	close(f2);
 	close(f3);
